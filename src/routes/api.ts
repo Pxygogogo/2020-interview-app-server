67683383a329d414ced1fdcd7e2f30ef8cd905a3
CaiZhuo <18847068680@163.com>
2020-02-15 13:27:30 +0800

    modify:unify the code style

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -1,7 +1,7 @@
 import KoaRouter from 'koa-router';
 import {DefaultState} from 'koa';
 import {CustomContext} from '../typings';
-import axios, {AxiosResponse} from 'axios'
+import axios from 'axios';
 
 const request = axios.create({
     baseURL: 'https://m.nowcoder.com',
@@ -9,51 +9,41 @@ const request = axios.create({
 });
 
 request.interceptors.response.use((res) => {
-    return res.data
+    return res.data;
 });
 
 const router = new KoaRouter<DefaultState, CustomContext>({
     prefix: '/api',
 });
 
-router.get('/hot-company', async (ctx, next) => {
-    ctx.body = await request.get('/weixin/mjdq/hot-company')
+router.get('/hot-company', async ctx => {
+    ctx.body = await request.get('/weixin/mjdq/hot-company');
 });
 
-router.get('/discuss', async (ctx, next) => {
+router.get('/discuss', async ctx => {
     const {page} = ctx.query;
-    ctx.body = await request.get(`/weixin/mjdq/discuss?page=${page}`);
-    await next();
-})
+    ctx.body = (await request.get(`/weixin/mjdq/discuss?page=${page}`)).data;
+});
 
-router.get('/search', async (ctx, next) => {
+router.get('/search', async ctx => {
     const {query, order, page} = ctx.query;
-    ctx.body = await request.get(`/weixin/mjdq/search?query=${query}&order=${order}&page=${page}`)
+    ctx.body = (await request.get(`/weixin/mjdq/search?query=${query}&order=${order}&page=${page}`)).data;
 });
 
-router.use(async (ctx, next) => {
+router.use(async ctx => {
     const {discussId} = ctx.query;
-    ctx.body = await request.get(`/discuss/get-discuss-info?discussId=${discussId}`)
-})
-
-router.use(async (ctx, next) => {
-    const {order = 1, pageSize = 20, entityId, entityType = 8} = ctx.query
-    ctx.body = await request.get(`/comment/list-v3?order=${order}&pageSize=${pageSize}&entityId=${entityId}&entityType=${entityType}`);
-})
-
-router.use(async (ctx, next) => {
-    const {entityId, entityType} = ctx.query;
-    ctx.body = await request.get(`/comment/wonderful/list?entityId=${entityId}&entityType=${entityType}`)
-})
-
-
-router.use((ctx, next) => {
-    if (ctx.body.code === 0) {
-        ctx.body = ctx.body.data;
-    } else {
-        throw new Error('官方接口请求出问题')
-    }
+    ctx.body = (await request.get(`/discuss/get-discuss-info?discussId=${discussId}`)).data;
 });
 
+router.use(async ctx => {
+    const {order = 1, pageSize = 20, entityId, entityType = 8} = ctx.query;
+    ctx.body = (await request.get(`/comment/list-v3?order=${order}&pageSize=${pageSize}&entityId=${entityId}&entityType=${entityType}`)).data;
+});
+
+router.use(async ctx => {
+    const {entityId, entityType} = ctx.query;
+    ctx.body = (await request.get(`/comment/wonderful/list?entityId=${entityId}&entityType=${entityType}`)).data;
+
+});
 
 export default router;

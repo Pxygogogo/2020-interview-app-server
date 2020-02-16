05e8b4484d8afefad9ba9fd3b5bceb6710f798a8
CaiZhuo <18847068680@163.com>
2020-02-16 22:09:16 +0800

    modify:fix api

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -1,6 +1,6 @@
 import KoaRouter from 'koa-router';
-import {DefaultState} from 'koa';
-import {CustomContext} from '../typings';
+import { DefaultState } from 'koa';
+import { CustomContext } from '../typings';
 import axios from 'axios';
 
 const request = axios.create({
@@ -15,35 +15,34 @@ request.interceptors.response.use((res) => {
 const router = new KoaRouter<DefaultState, CustomContext>({
     prefix: '/api',
 });
-
+//热门企业
 router.get('/hot-company', async ctx => {
     ctx.body = await request.get('/weixin/mjdq/hot-company');
 });
-
+//讨论
 router.get('/discuss', async ctx => {
-    const {page} = ctx.query;
+    const { page } = ctx.query;
     ctx.body = (await request.get(`/weixin/mjdq/discuss?page=${page}`)).data;
 });
-
+//搜索
 router.get('/search', async ctx => {
-    const {query, order, page} = ctx.query;
+    const { query, order, page } = ctx.query;
     ctx.body = (await request.get(`/weixin/mjdq/search?query=${query}&order=${order}&page=${page}`)).data;
 });
-
-router.use(async ctx => {
-    const {discussId} = ctx.query;
+//企业面经
+router.use('/company-expierence', async ctx => {
+    const { page } = ctx.query
+    ctx.body = (await request.get(`/weixin/mjdq/discuss/community/134?page=${page}`)).data;
+})
+//面经详情
+router.use('/discuss-info', async ctx => {
+    const { discussId } = ctx.query;
     ctx.body = (await request.get(`/discuss/get-discuss-info?discussId=${discussId}`)).data;
 });
-
-router.use(async ctx => {
-    const {order = 1, pageSize = 20, entityId, entityType = 8} = ctx.query;
+//面经评论
+router.use('/detail', async ctx => {
+    const { order = 1, pageSize = 20, entityId, entityType = 8 } = ctx.query;
     ctx.body = (await request.get(`/comment/list-v3?order=${order}&pageSize=${pageSize}&entityId=${entityId}&entityType=${entityType}`)).data;
-});
-
-router.use(async ctx => {
-    const {entityId, entityType} = ctx.query;
-    ctx.body = (await request.get(`/comment/wonderful/list?entityId=${entityId}&entityType=${entityType}`)).data;
-
 });
 
 export default router;
